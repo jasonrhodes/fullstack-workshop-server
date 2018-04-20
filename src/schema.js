e5263228556e694661e5d0a3669e9a66e76bf42a
Peggy Rayzis <peggyrayzis@gmail.com>
2018-04-20 01:17:54 -0600

    Server completed

--- src/schema.js
+++ src/schema.js
@@ -32,42 +32,15 @@ Part 2: Fill in some queries and mutations.
 
 module.exports = `
   type Query {
-    movie(id: ID): Movie
     movies(sort: SORT_TYPE, page: Int): [Movie]!
-    likes: [Movie]
   }
 
   type Mutation {
-    toggleLike(id: ID!): Movie
     login(email: String!): String
   }
 
   enum SORT_TYPE {
     POPULARITY
     RELEASE_DATE
-  }
-
-  type Movie {
-    id: ID
-    title: String
-    overview: String
-    score: Float
-    voteCount: Int
-    popularity: Float
-    poster(size: Int): String
-    genres: [String]!
-    releaseDate: String
-    cast: [Cast]!
-    isLiked: Boolean
-    runtime: Int
-  }
-
-
-  type Cast {
-    id: ID
-    name: String!
-    photo(size: Int): String
-    gender: String
-    character: String
   }
 `;
